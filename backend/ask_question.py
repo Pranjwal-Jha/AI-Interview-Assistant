734bff4974192cd9558e47dc1464440cd47eb17c
Pranjwal-Jha <pranjwaljha2005@gmail.com>
2025-06-21 21:44:09 +0530

    Worked on toolnode

--- backend/ask_question.py
+++ backend/ask_question.py
@@ -1,19 +1,19 @@
-from langchain_core.messages import AIMessage,BaseMessage, HumanMessage
-from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
-from langgraph.graph import StateGraph
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import Field,BaseModel
-from gemini_llm import llm
+from langchain_core.tools import tool
 from typing import cast
 
 class Question(BaseModel):
     name:str=Field(...,description="Name of a LeetCode question in lowercase joined with '-'")
     id:str=Field(...,description="question ID of the LeetCode question")
+llm=ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
-def leetcode(question:str):
+@tool
+def leetcode(question:str)->dict:
+    """get a leetcode question and it's corresponding id by sending a topic of your choice"""
     parser=PydanticOutputParser(pydantic_object=Question)
-
     template = ChatPromptTemplate.from_messages([
         ("system", """You are a helpful AI that gives a single LeetCode question on the topic {topic}.
     Respond ONLY with JSON in the following format:
@@ -27,11 +27,13 @@ def leetcode(question:str):
         response=llm.invoke(prompt)
         response_string=cast(str,response.content)
         output=parser.parse(response_string)
+        print(output)
         return {"question":output.name,"id":output.id}
     except Exception as e:
         print(e)
-        return None
+        return {}
 
+leetcode.invoke("dp")
 # leetcode("dp")
 
 # while True:
--- backend/llm_response.py
+++ backend/llm_response.py
@@ -1,20 +1,23 @@
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessage, HumanMessage
 from langgraph.constants import END, START
 from langgraph.graph import StateGraph
+from langgraph.prebuilt import ToolNode
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langgraph.checkpoint.sqlite import SqliteSaver
 import sqlite3
+from ask_question import leetcode
 from parse_pdf import greet_candidate
 from shared import InterviewChat
 from gemini_llm import llm
 sql_conn=sqlite3.connect("checkpoint.sqlite",check_same_thread=False)
 memory=SqliteSaver(sql_conn)
+tool_node=ToolNode([leetcode])
 
 def get_gemini_response(state:InterviewChat):
     messages=state["messages"]
     resume_text=state["resume"]
     interview_prompt=ChatPromptTemplate.from_messages([
-        ("system","""You are an AI interviewer currently conducting an interview with the candidate. Briefly acknowledge the candidate’s previous answers or progress so far, mentioning specifics when possible. Then, ask a follow-up or next question that logically continues the conversation based on the candidate’s responses or resume.
+        ("system","""You are an AI interviewer currently conducting an interview with the candidate. Briefly acknowledge the candidate’s previous answers or progress so far, mentioning specifics when possible. Then, ask a follow-up or next question that logically continues the conversation based on the candidate’s responses or resume. If a user asks for leetcode question you use the tools to ask him a leetcode question about a topic of your choice
         Example:
         'Thanks for your explanation on the last question about system design. You mentioned experience with microservices in your resume — can you elaborate on how you handled data consistency across those services?'
         Keep the tone professional but conversational, and stay focused on assessing the candidate’s skills and experience
@@ -33,32 +36,25 @@ def get_gemini_response(state:InterviewChat):
     }
 
 def should_continue(state:InterviewChat):
-    """Determine if you want to ask more questions to the User or want to end the Interview"""
-    messages = state["messages"]
-    if len(messages)>0 and isinstance(messages[-1],HumanMessage):
-        return "get_gemini_response"
+    """Determine if you want to ask a leetcode question or continue with the interview"""
+    last_message=state["messages"][-1]
+    if isinstance(last_message,AIMessage) and last_message.tool_calls:
+        return "tools"
     else:
         return END
 
 graph=StateGraph(InterviewChat)
 graph.add_node("greet_candidate",greet_candidate)
 graph.add_node("get_gemini_response",get_gemini_response)
+graph.add_node("tools",tool_node)
 graph.add_edge(START,"greet_candidate")
-graph.add_conditional_edges(
-    "greet_candidate",
-    should_continue,
-    {
-        "get_gemini_response":"get_gemini_response",
-        END:END
-    }
-)
+graph.add_edge("greet_candidate",END)
 graph.add_conditional_edges(
     "get_gemini_response",
     should_continue,
     {
-        "get_gemini_response":"get_gemini_response",
+        "tools":"tools",
         END:END
     }
 )
-# graph.add_edge("greet_candidate",END)
 compiled_graph=graph.compile(checkpointer=memory)
